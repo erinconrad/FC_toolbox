24f16c459ab6c8a2fc1ebf283e1a822ba258db9b
Erin <erincconrad@gmail.com>
2021-09-17 07:35:10 -0400

    try catch loop for server errors

--- ieeg_stuff/download_ieeg_data.m
+++ ieeg_stuff/download_ieeg_data.m
@@ -1,61 +1,82 @@
 function data = download_ieeg_data(fname, login_name, pwfile, run_times, extras)
 
-session = IEEGSession(fname, login_name, pwfile);
-channelLabels = session.data.channelLabels;
-nchs = size(channelLabels,1);
+attempt = 1;
 
-% get fs
-data.fs = session.data.sampleRate;
+% Wrap data pulling attempts in a while loop
+while 1
+    
+    try
+        session = IEEGSession(fname, login_name, pwfile);
+        channelLabels = session.data.channelLabels;
+        nchs = size(channelLabels,1);
 
-% Convert times to indices
-run_idx = round(run_times(1)*data.fs):round(run_times(2)*data.fs);
+        % get fs
+        data.fs = session.data.sampleRate;
 
-if ~isempty(run_idx)
-    % Break the number of channels in half to avoid wacky server errors
-    values1 = session.data.getvalues(run_idx,1:floor(nchs/4));
-    values2 = session.data.getvalues(run_idx,floor(nchs/4)+1:floor(2*nchs/4));
-    values3 = session.data.getvalues(run_idx,floor(2*nchs/4)+1:floor(3*nchs/4)); 
-    values4 = session.data.getvalues(run_idx,floor(3*nchs/4)+1:nchs); 
-    
-    values = [values1,values2,values3,values4];
-else
-    values = [];
-end
+        % Convert times to indices
+        run_idx = round(run_times(1)*data.fs):round(run_times(2)*data.fs);
 
-data.values = values;
+        if ~isempty(run_idx)
+            % Break the number of channels in half to avoid wacky server errors
+            values1 = session.data.getvalues(run_idx,1:floor(nchs/4));
+            values2 = session.data.getvalues(run_idx,floor(nchs/4)+1:floor(2*nchs/4));
+            values3 = session.data.getvalues(run_idx,floor(2*nchs/4)+1:floor(3*nchs/4)); 
+            values4 = session.data.getvalues(run_idx,floor(3*nchs/4)+1:nchs); 
 
-if extras == 1
-    
-    % get file name
-    data.file_name = session.data.snapName;
-    
-    % Get ch labels
-    data.chLabels = channelLabels;
-    
-    % get duration (convert to seconds)
-    data.duration = session.data.rawChannels(1).get_tsdetails.getDuration/1e6;
-  
-    % Get annotations
-    n_layers = length(session.data.annLayer);
-    
-    for ai = 1:n_layers
-        a=session.data.annLayer(ai).getEvents(0);
-        n_ann = length(a);
-        for i = 1:n_ann
-            event(i).start = a(i).start/(1e6);
-            event(i).stop = a(i).stop/(1e6); % convert from microseconds
-            event(i).type = a(i).type;
-            event(i).description = a(i).description;
+            values = [values1,values2,values3,values4];
+        else
+            values = [];
         end
-        ann.event = event;
-        ann.name = session.data.annLayer(ai).name;
-        data.ann(ai) = ann;
+
+        data.values = values;
+
+        if extras == 1
+
+            % get file name
+            data.file_name = session.data.snapName;
+
+            % Get ch labels
+            data.chLabels = channelLabels;
+
+            % get duration (convert to seconds)
+            data.duration = session.data.rawChannels(1).get_tsdetails.getDuration/1e6;
+
+            % Get annotations
+            n_layers = length(session.data.annLayer);
+
+            for ai = 1:n_layers
+                a=session.data.annLayer(ai).getEvents(0);
+                n_ann = length(a);
+                for i = 1:n_ann
+                    event(i).start = a(i).start/(1e6);
+                    event(i).stop = a(i).stop/(1e6); % convert from microseconds
+                    event(i).type = a(i).type;
+                    event(i).description = a(i).description;
+                end
+                ann.event = event;
+                ann.name = session.data.annLayer(ai).name;
+                data.ann(ai) = ann;
+            end 
+
+        end
+        
+        % break out of while loop
+        break
+        
+    % If any error, try again (this is because there are frequent random
+    % server errors). Note this will be problematic if I have a real coding
+    % error.
+    catch
+        attempt = attempt + 1;
+        fprintf('Failed to retrieve ieeg.org data, trying again (attempt %d)\n',attempt); 
+        
     end
-    
 end
 
 %% Delete session
 session.delete;
+clearvars -except data
+clearjava
 
 
 end
