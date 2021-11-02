3490e5092641d1cec2905176cf7bb958a39bc36d
Erin <erincconrad@gmail.com>
2021-11-02 10:03:59 -0400

    rm spikes in szs from summ file

--- seizure_info/seizure_times/rm_spikes_in_seizures.m
+++ seizure_info/seizure_times/rm_spikes_in_seizures.m
@@ -7,6 +7,11 @@ function [gdf,n_ictal] = rm_spikes_in_seizures(gdf,fs,run_start,sz_times)
 % gdf(:,2) are the indices into the run of the spikes (need to convert to
 % seconds relative to file start)
 
+if isempty(sz_times)
+    n_ictal = 0;
+    return
+end
+
 sp_indices = gdf(:,2);
 sp_times = (sp_indices-1)/fs + run_start;
 
