1463ef19598f0f242846db21212d445aa7e9669d
Erin <erincconrad@gmail.com>
2021-11-03 16:16:26 -0400

    rm spikes in szs from summ file

---
+++ analyses/seizure_time/ar_model_szs_sleep.m
@@ -0,0 +1,4 @@
+function ar_model_szs_sleep
+
+
+end
--- analyses/seizure_time/seizure_time_histogram.m
+++ analyses/seizure_time/seizure_time_histogram.m
@@ -32,8 +32,12 @@ addpath(genpath(scripts_folder));
 listing = dir([int_folder,'*.mat']);
 npts = length(listing);
 
+%% AD validation
+[roc,auc,disc] = ad_validation;
+
 %% initialize
 all_pts_spikes_bins = nan(npts,nbins);
+all_pts_sleep_bins = nan(npts,nbins);
 
 for p = 1:npts
     
@@ -48,12 +52,21 @@ for p = 1:npts
     spikes = summ.spikes;
     sz_times = summ.sz_times;
     times = summ.times;
+    ad = summ.ad;
     
     %% Find and remove non-intracranial
     ekg = find_non_intracranial(labels);
     
     spikes = spikes(~ekg,:);
     avg_spikes = nanmean(spikes,1);
+    ad = ad(~ekg,:);
+    ad = nanmean(ad,1);
+    
+    %% Determine "wake" and "sleep" times
+    % normalized ad
+    ad_norm = (ad - nanmedian(ad))./iqr(ad);
+    wake = ad_norm > disc;
+    sleep = ad_norm <= disc;
     
     
     %% bin times around seizures
@@ -75,9 +88,11 @@ for p = 1:npts
         xlim([times(1) times(end)])
     end
     
-    %% Get spikes in those times
+    %% Get spikes and sleep in those times
     
     sp_bins = nan(size(bins));
+    sleep_in_bins = nan(size(bins));
+
     for s = 1:size(bins,1)
         
         curr_bins = bins(s,:);
@@ -89,8 +104,13 @@ for p = 1:npts
         
         % get the spikes for those in range
         out_bins = [nan(1,sum(too_early)),avg_spikes(curr_bins(bins_in_range)),nan(1,sum(too_late))];
-        
         sp_bins(s,:) = out_bins;
+        
+        
+        % sleep bins
+        sleep_in_bins(s,:) = [nan(1,sum(too_early)),...
+            sleep_in_bins(curr_bins(bins_in_range)),nan(1,sum(too_late))];
+
     end
     
     %% test plots
@@ -111,22 +131,27 @@ for p = 1:npts
     
     %% Average over seizures
     sz_avg_sp_bins = nanmean(sp_bins,1);
+    sleep_in_bins = nanmean(sleep_in_bins,1);
+    
     
     %% Add to patient matrix
     all_pts_spikes_bins(p,:) = sz_avg_sp_bins;
+    all_pts_sleep_bins(p,:) = sleep_in_bins;
 end
 
 figure
 sp_bins = nanmean(all_pts_spikes_bins,1);
+sleep_bins = nanmean(all_pts_sleep_bins,1);
 times = linspace(-surround_hours,surround_hours,length(sp_bins));
-plot(times,sp_bins,'k');
+psp = plot(times,sp_bins);
 hold on
+psl = plot(times,sleep_bins);
 title('Spike rate surrounding seizure onset')
 xlabel('Hours')
-ylabel('Spikes/elecs/min')
 xlim([-surround_hours surround_hours])
 yl = ylim;
 plot([0 0],yl,'r--')
+legend([psp,psl],{'Spikes/elec/min','Proportion asleep'});
 ylim(yl)
 print([out_folder,'histogram',rm_cluster_text],'-dpng')
 close(gcf)
