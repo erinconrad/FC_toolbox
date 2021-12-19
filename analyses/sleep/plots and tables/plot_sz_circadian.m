3bf3f68086fbeaa7941d9575cc8e7c445f7c5de1
Erin <erincconrad@gmail.com>
2021-12-18 21:20:11 -0500

    sz circadian

---
+++ analyses/sleep/plots and tables/plot_sz_circadian.m
@@ -0,0 +1,43 @@
+function plot_sz_circadian
+
+%% Parameters
+plot_type = 'scatter';
+nblocks = 6;
+myColours = [0, 0.4470, 0.7410;...
+    0.8500, 0.3250, 0.0980;...
+    0.4660, 0.6740, 0.1880;...
+    0.4940, 0.1840, 0.5560;...
+    0.6350, 0.0780, 0.1840];
+
+
+
+locations = fc_toolbox_locs;
+addpath(genpath(locations.script_folder))
+results_folder = [locations.main_folder,'results/'];
+out_folder = [results_folder,'analysis/sleep/'];
+
+%% Load out file and get roc stuff
+out = load([out_folder,'out.mat']);
+out = out.out;
+
+%% Unpack substructures
+unpack_any_struct(out);
+out_folder = [results_folder,'analysis/sleep/'];
+
+
+figure
+
+nexttile
+periods = sz_circ_out.periods;
+iqr_psd = sz_circ_out.iqr_psd;
+median_psd = sz_circ_out.median_psd;
+mp = shaded_error_bars(periods,median_psd,iqr_psd,[0 0 0]);
+
+nexttile
+pre_wake = sz_circ_out.pre_wake;
+for i = 1:length(pre_wake)
+    plot(i,sum(pre_wake==1)/(sum(pre_wake==1) + sum(pre_wake==0)),'o')
+    hold on
+end
+
+end
