622baacd3fe7f1a7deaa2c551828583d8c1a9c26
Erin <erincconrad@gmail.com>
2022-02-27 14:01:53 -0500

    why didnt it work

--- analyses/sleep/main_sleep_analyses/all_pt_psd.m
+++ analyses/sleep/main_sleep_analyses/all_pt_psd.m
@@ -18,7 +18,7 @@ main{2} = main_lats;
 %% Get file locs
 locations = fc_toolbox_locs;
 results_folder = [locations.main_folder,'results/'];
-int_folder = [results_folder,'analysis/intermediate/'];
+int_folder = [results_folder,'analysis/backup_intermediate_Feb26_good_spikes/'];
 out_folder = [results_folder,'analysis/sleep/'];
 if ~exist(out_folder,'dir')
     mkdir(out_folder)
