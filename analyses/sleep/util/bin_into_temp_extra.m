c7c88f20abecb4fd09ac7e5cfc5e50722fa39746
Erin <erincconrad@gmail.com>
2022-03-03 11:03:49 -0500

    prepping for spikes anat

---
+++ analyses/sleep/util/bin_into_temp_extra.m
@@ -0,0 +1,15 @@
+function out_locs = bin_into_temp_extra(locs)
+
+out_locs = cell(size(locs));
+
+for i = 1:length(locs)
+    curr = locs{i};
+    
+    if contains(curr,'temporal')
+        out_locs{i} = 'temporal';
+    elseif contains(curr,'other cortex')
+        out_locs{i} = 'other cortex';
+    end
+end
+
+end
---
+++ analyses/sleep/util/count_pts_with_elec_anat.m
@@ -0,0 +1,6 @@
+function count_pts_with_elec_anat
+
+
+
+
+end
