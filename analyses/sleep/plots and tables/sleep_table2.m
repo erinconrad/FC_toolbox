ea105b7f847d66e1690572bc03b18bc92219fefe
Erin <erincconrad@gmail.com>
2022-03-08 19:33:17 -0500

    added missing file times

--- analyses/sleep/plots and tables/sleep_table2.m
+++ analyses/sleep/plots and tables/sleep_table2.m
@@ -4,7 +4,8 @@ function sleep_table2
 locations = fc_toolbox_locs;
 results_folder = [locations.main_folder,'results/'];
 out_folder = [results_folder,'analysis/sleep/'];
-int_folder = [results_folder,'analysis/intermediate/'];
+%int_folder = [results_folder,'analysis/intermediate/'];
+int_folder = [results_folder,'analysis/backup_intermediate_Feb26_good_spikes/'];
 if ~exist(out_folder,'dir')
     mkdir(out_folder)
 end
