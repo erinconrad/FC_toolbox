4a25e8d56c39f172d8444f1c465afb091b795505
Erin <erincconrad@gmail.com>
2022-03-16 13:48:48 -0400

    prep outcome analysis

--- atlas/build_fc_atlas.m
+++ atlas/build_fc_atlas.m
@@ -1,7 +1,7 @@
 function build_fc_atlas
 
 %% Parameters
-atlas = 'aal_bernabei'; %'brainnetome';
+atlas = 'brainnetome'; %'aal_bernabei'; %
 too_many_spikes = 1; % 1 spikes/elecs/min
 
 %% Get file locs
