b4c5dfa4c381358f0a6fcda1a4cb2da561088eef
Erin <erincconrad@gmail.com>
2022-05-20 14:48:44 -0400

    made code friendlier for github downloads

--- analyses/outcome_prediction/plots/paper_plots/main_ieeg_biases.m
+++ analyses/outcome_prediction/plots/paper_plots/main_ieeg_biases.m
@@ -1,13 +1,18 @@
-function main_ieeg_biases(n_test_train_splits)
+function main_ieeg_biases(n_test_train_splits,doing_from_github)
 
 % seed random number generator (for consistency in the random testing/training splits)
 rng(0) 
 
-% 1 means doing from github so skip some conceptual plots and Table 1 that require looking at larger datasets
-doing_from_github = 1;
+doing_from_github = 1
 
+% how many testing/training splits
 if ~exist('n_test_train_splits','var')
     n_test_train_splits = 1e3;
+end
+
+% 1 means doing from github so skip some conceptual plots and Table 1 that require looking at larger datasets
+if ~exist('doing_from_github','var')
+    doing_from_github = 1;
 end
 
 %% Get file locs and set script path
