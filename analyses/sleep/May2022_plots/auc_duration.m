eded14f8a1a88d67fbab1fe290ae0cdf5d9d8ba3
Erin <erincconrad@gmail.com>
2022-05-26 14:17:29 -0400

    new pre-post stuff

--- analyses/sleep/May2022_plots/auc_duration.m
+++ analyses/sleep/May2022_plots/auc_duration.m
@@ -18,6 +18,7 @@ addpath(genpath(locations.script_folder))
 script_folder = locations.script_folder;
 results_folder = [locations.main_folder,'results/'];
 out_folder = [results_folder,'analysis/sleep/'];
+time_roc_folder = [out_folder,'time_roc/'];
 if ~exist(time_roc_folder,'dir')
     mkdir(time_roc_folder)
 end
