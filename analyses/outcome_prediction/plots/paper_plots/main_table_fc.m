241ded9165a01ef622bd927066785b6631cc80a1
Erin <erincconrad@gmail.com>
2022-06-21 08:47:01 -0400

    fixed a typo messing up gray matter analysis for sleep

--- analyses/outcome_prediction/plots/paper_plots/main_table_fc.m
+++ analyses/outcome_prediction/plots/paper_plots/main_table_fc.m
@@ -11,11 +11,11 @@ int_folder = [results_folder,'analysis/intermediate/']; % this includes patients
 % add script folder to path
 scripts_folder = locations.script_folder;
 addpath(genpath(scripts_folder));
-model_folder = locations.paper_data_folder;
+model_folder = locations.paper_plot_folder;
 
 
 % Spike-fc corr
-corr_out = load([model_folder,'spikes_out.mat']);
+corr_out = load([model_folder,'spikes_analysis.mat']);
 corr_out = corr_out.nout;
 any_locs = corr_out.pts_with_any_locs;
 names = corr_out.names;
