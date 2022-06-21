4832d15f701e3c452d870a06b874a9f773ca2118
Erin <erincconrad@gmail.com>
2022-06-21 08:44:34 -0400

    fixed a typo messing up gray matter analysis for sleep

--- analyses/outcome_prediction/plots/paper_plots/main_table_fc.m
+++ analyses/outcome_prediction/plots/paper_plots/main_table_fc.m
@@ -15,7 +15,7 @@ model_folder = locations.paper_data_folder;
 
 
 % Spike-fc corr
-corr_out = load([model_folder,'spike_out.mat']);
+corr_out = load([model_folder,'spikes_out.mat']);
 corr_out = corr_out.nout;
 any_locs = corr_out.pts_with_any_locs;
 names = corr_out.names;
