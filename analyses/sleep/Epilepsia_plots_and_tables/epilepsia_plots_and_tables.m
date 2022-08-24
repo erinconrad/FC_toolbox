9ccf500923af01129a832395ab1a8d18c99bb0f8
Erin <erincconrad@gmail.com>
2022-08-24 16:13:54 -0400

    prepping 3 hour preictal

--- analyses/sleep/Epilepsia_plots_and_tables/epilepsia_plots_and_tables.m
+++ analyses/sleep/Epilepsia_plots_and_tables/epilepsia_plots_and_tables.m
@@ -12,9 +12,9 @@ epilepsia_supplemental_fig1
 
 epilepsia_supplemental_fig2
 
-epilepsia_supplemental_fig3
+%epilepsia_supplemental_fig3
 
-epilepsia_supplemental_table1
+%epilepsia_supplemental_table1
 
 %epilepsia_table1 % needs to be done on computer with individual patient
 %files
