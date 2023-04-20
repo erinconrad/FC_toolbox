3d6a7717287d236dffd09754d253c8b743248f61
Erin <erincconrad@gmail.com>
2023-04-20 13:54:19 -0400

    get atlas parcellations

---
+++ create_pt_struct/add_atlas_parcellations.m
@@ -0,0 +1,91 @@
+function add_atlas_parcellations
+
+%% Get file locs
+locations = fc_toolbox_locs;
+results_folder = [locations.main_folder,'results/'];
+bids_folder = '/data/Human_Data/CNT_iEEG_BIDS/';
+data_folder = [locations.main_folder,'data/'];
+
+% add script folder to path
+scripts_folder = locations.script_folder;
+addpath(genpath(scripts_folder));
+
+%% Get pt file
+pt = load([data_folder,'pt.mat']);
+pt = pt.pt;
+
+%% Loop over patients and get locs
+for p = 1:length(pt)
+    name = pt(p).name;
+    rid = pt(p).rid;
+    
+    elec_names = pt(p).elecs_native.elec_names;
+
+    % Load the relevant atlas parcellation files
+    if rid < 100
+        rid_text = ['sub-RID',sprintf('00%d',rid)];
+    else
+        rid_text = ['sub-RID',sprintf('0%d',rid)];
+    end
+    
+    pt_folder = [bids_folder,rid_text,'/'];
+    
+
+    module3_folder = [pt_folder,'derivatives/ieeg_recon/module3/'];
+
+    atropos_file = [rid_text,'_ses-research3T_space-T00mri_atlas-atropos_radius-2_desc-vox_coordinates.csv'];
+    dkt_file = [rid_text,'_ses-research3T_space-T00mri_atlas-DKTantspynet_radius-2_desc-vox_coordinates.csv'];
+
+    if ~exist([module3_folder,atropos_file],'file')
+        continue
+    end
+
+    aT = readtable([module3_folder,atropos_file]);
+    dT = readtable([module3_folder,dkt_file]);
+
+    % get elec names
+    a_name = aT.name;
+    d_name = dT.name;
+
+    % get xyz
+    a_xyz = [aT.x aT.y aT.z];
+    d_xyz = [dT.x dT.y dT.z];
+
+    % get indices
+    a_idx = aT.index;
+    d_idx = dT.index;
+
+    % get labels
+    a_label = aT.label;
+    d_label = dT.label;
+
+    % Reconcile electrode names and re-order as needed
+    assert(isequal(a_name,d_name))
+    assert(isequal(a_xyz,d_xyz))
+
+    [Lia,Locb] = ismember(a_name,elec_names);
+    assert(isequal(a_name(Locb(Lia)),elec_names))
+
+    a_label = a_label(Locb(Lia));
+    d_label = d_label(Locb(Lia));
+    a_xyz = a_xyz(Locb(Lia));
+    d_xyz = d_xyz(Locb(Lia));
+    a_idx = a_idx(Locb(Lia));
+    d_idx = d_idx(Locb(Lia));
+    
+    % fill up
+    pt(p).elecs_native.atropos.names = elec_names;
+    pt(p).elecs_native.atropos.xyz = a_xyz;
+    pt(p).elecs_native.atropos.idx = a_idx;
+    pt(p).elecs_native.atropos.label = a_label;
+
+    pt(p).elecs_native.dkt.names = elec_names;
+    pt(p).elecs_native.dkt.xyz = d_xyz;
+    pt(p).elecs_native.dkt.idx = d_idx;
+    pt(p).elecs_native.dkt.label = d_label;
+    
+    save([data_folder,'pt.mat'],'pt');
+
+end
+
+end
