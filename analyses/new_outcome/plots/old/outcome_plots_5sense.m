20737dd7b07801138db2e118d1c5da121ec3cfea
Erin <erincconrad@gmail.com>
2023-08-11 11:44:58 -0400

    cut more words from results

--- analyses/new_outcome/plots/old/outcome_plots_5sense.m
+++ analyses/new_outcome/plots/old/outcome_plots_5sense.m
@@ -34,7 +34,8 @@ for ir = 1:length(which_refs)
         fid = fopen(fname,'a');
         fprintf(fid,'<br><u><i>Concordance between spike-predicted laterality and surgical laterality is higher for patients with good surgical outcomes</i></u></br>');
         
-      
+        fprintf(fid,['We examined the one-year surgical outcomes of patients who underwent resection '...
+            'or laser ablation, studying both Engel and ILAE outcome classifications.']);
     end
     
     %% Load the model file
@@ -108,8 +109,8 @@ for ir = 1:length(which_refs)
     
     %% Initialize figure
     figure
-    set(gcf,'position',[1 1 1000 1000])
-        tiledlayout(2,2,"TileSpacing",'tight','padding','tight')
+    set(gcf,'position',[1 1 1400 1000])
+        tiledlayout(2,3,"TileSpacing",'tight','padding','tight')
     
     % Prep stats for text
     good_bad = nan(2,2); % engel, ilae; good, bad
@@ -252,7 +253,7 @@ for ir = 1:length(which_refs)
     
         % investigating the patients with high predicted concordant
         % laterality but poor outcome
-        if 0
+        if 1
             table([T.names(bad_outcome&left_surg);T.names((bad_outcome&right_surg))],[left_scores(bad_outcome&left_surg);right_scores(bad_outcome&right_surg)])
         end
         % HUP138 (0.81) had a high modeled
@@ -260,7 +261,96 @@ for ir = 1:length(which_refs)
         % had an ablation, and no repeat surgical evaluation. 
     
     
+        %% C and F: Comparison between 5-Sense and 5-Sense + this model
+        % Prep info about model
+        scores = concordant_lat_scores;
+        outcome_for_mdl = nan(length(scores),1);
+        outcome_for_mdl(strcmp(outcome_bin,'good')) = 1; outcome_for_mdl(strcmp(outcome_bin,'bad')) = 0;
+        nan_rows =  isnan(scores) | cellfun(@isempty,outcome_bin);
+        non_nan_names = names(~nan_rows);
+    
+        % Get 5 sense score
+        outT = five_sense_calculator(0,non_nan_names);
+    
+        % Reconcile 5-sense patients with my main table
+        names_5sense = outT.names;
+        prob_5sense = outT.prob;
+        reconciled_prob = nan(length(names),1);
+
+        % add info for the two musc patients with 5-sense scores (copied
+        % these probs from LEN table)
+        names_5sense = [names_5sense;{'MP0006'};{'MP0011'}];
+        prob_5sense = [prob_5sense;56.8;29.2];
+    
+        for i = 1:length(names)
+            % get row in the other outcome table corresponding to this name
+            curr_name = names{i};
+            row = strcmp(curr_name,names_5sense);
         
+            if sum(row)~=1, continue; end
+        
+            reconciled_prob(i) = prob_5sense(row);
+        end
+    
+        newT = table(names,outcome_bin,outcome_for_mdl,surg,left_surg,right_surg,reconciled_prob,left_scores,right_scores,scores);
+        newT(isnan(newT.scores) | cellfun(@isempty,newT.outcome_bin),:) = []; % remove patients without model scores and patients without outcomes
+        newT.outcome_for_mdl = logical(newT.outcome_for_mdl);
+    
+        % combined spike and 5-sense model to predict outcome
+        probs = simple_loo_cv(newT,'outcome_for_mdl ~ reconciled_prob + scores','binomial');
+        [X,Y,~,AUC] = perfcurve(newT.outcome_bin,probs,'good');
+    
+        % just 5-sense model to predict outcome
+        simple_probs = simple_loo_cv(newT,'outcome_for_mdl ~ reconciled_prob','binomial');
+        [simple_X,simple_Y,~,simple_AUC] = perfcurve(newT.outcome_bin,simple_probs,'good');
+    
+        % DeLong test comparing simple to complicated model
+        pred = [probs,simple_probs];
+        target = newT.outcome_for_mdl;
+        % rm nans
+        nan_rows = any(isnan(pred),2);
+        pred(nan_rows,:) = [];
+        target(nan_rows) = [];
+    
+        [ W ] = wilcoxon( pred, target );
+        assert(abs(W(1)-AUC)<5e-3 && abs(W(2)-simple_AUC)<5e-3)
+        
+        alpha = 0.05; % significance level
+    
+        [ S, S10, S01, V10, V01, theta ] = wilcoxonCovariance(pred,target);
+        [alt_auc1, auc_ci1] = wilcoxonCI(pred(:,1),target, alpha);
+        [alt_auc2, auc_ci2] = wilcoxonCI(pred(:,2),target, alpha);
+        assert(abs(alt_auc1-AUC)<1e-3 && abs(alt_auc2-simple_AUC)<1e-3)
+        L = [1,-1]; % must always sum to 0
+        
+        [ thetaP, thetaCI ] = wilcoxonConfidence(L, S, theta, alpha );
+        % I should check that this gives the same answer as R -> I checked and
+        % it did give the same answer as roc.test in R gives, and so I think
+        % this is correct (although not significant)
+    
+        %writetable(table(target,pred),[plot_folder,'delong_test.csv'])
+        auc_stats(io,:) = [simple_AUC,auc_ci2,AUC,auc_ci1,thetaP,size(target,1)];
+    
+    
+        %% do the plot
+        nexttile
+        cp = plot(X,Y,'linewidth',2);
+        hold on
+        sp = plot(simple_X,simple_Y,'linewidth',2);
+        plot([0 1],[0 1],'k--','linewidth',2)
+        xlabel('False positive rate')
+        ylabel('True positive rate')
+        legend([cp,sp],{sprintf('Model + pre-implant data: AUC = %1.2f',AUC),...
+            sprintf('Pre-implant data only: AUC = %1.2f',simple_AUC)},...
+            'location','southeast','fontsize',15)
+        title('Outcome prediction')
+        set(gca,'fontsize',15)
+
+        if 0
+            newT
+            % this is a nice table to double check numbers
+        end
+    
     
     end
     
@@ -280,29 +370,53 @@ for ir = 1:length(which_refs)
         
         fprintf(fid,[' We hypothesized that patients with a good surgical outcome would have a '...
             'higher modeled probability of SOZ laterality concordant with the side of surgery. '...
-            'We identified the spike rate model corresponding to the side of surgery. '...
+            'We identified the spike rate model corresponding to the side of surgery (e.g., for '...
+            'patients who underwent left-sided surgery, we used the model predicting left vs. right/bilateral SOZ). '...
             'Mean concordant model probability was significantly higher in patients with good Engel '...
             'outcomes (mean (SD) %1.2f (%1.2f)) than in patients with poor '...
             'Engel outcomes (%1.2f (%1.2f)) (<i>t</i>(%d) = %1.1f, %s) (Fig. 4B), and '...
-            'in patients with good ILAE outcomes (%1.2f (%1.2f)) than '...
+            'win patients with good ILAE outcomes (%1.2f (%1.2f)) than '...
             'in patients with poor ILAE outcomes (%1.2f (%1.2f)) (<i>t</i>(%d) = %1.1f, %s) (Fig. 4E). '...
             'Together, these results suggest that a model trained to predict the SOZ using spike rate '...
-            'asymmetry also predicts surgical outcome.'],...
+            'asymmetry also predicts surgical outcome.</p>'],...
             prob_stats(1,1),prob_stats(1,2),prob_stats(1,3),prob_stats(1,4),prob_stats(1,5),prob_stats(1,6),...
             get_p_html(prob_stats(1,7)),...
             prob_stats(2,1),prob_stats(2,2),prob_stats(2,3),prob_stats(2,4),prob_stats(2,5),prob_stats(2,6),...
             get_p_html(prob_stats(2,7)));
         
-      
-        fprintf(fid,[' Results were similar when we used spikes detected in bipolar and machine references (Fig. S4 and S5).</p>']);
+        fprintf(fid,['<p>We also asked if spike rate asymmetry adds additional information to '...
+            'predict surgical outcome beyond what is known using pre-implant data alone. '...
+            'We used the recently published 5-SENSE score to estimate the probability of a single focal '...
+            'seizure generator based on the pre-implant data. We used leave-one-patient-out cross-validation '...
+            'to predict good vs. poor surgical outcome using a logistic regression model with the 5-SENSE score '...
+            'as the only predictor. The 5-SENSE score predicted both Engel and ILAE outcomes (AUC = %1.2f '...
+            '(95%% CI %1.2f-%1.2f) and %1.2f (%1.2f-%1.2f), respectively). We next applied the same '...
+            'cross-validation approach to a logistic '...
+            'regression model incorporating both the 5-SENSE score and the modeled probability of concordant laterality. '...
+            'The combined model had similar AUCs to the simple model for predicting both Engel and ILAE outcomes (AUC = '...
+            '%1.2f (%1.2f-%1.2f) and %1.2f (%1.2f-%1.2f), respectively; DeLong test: %s for Engel outcome '...
+            'and %s for ILAE outcome) (Fig. 4C and F). However, this analysis was limited by '...
+            'the small number of patients with available outcome data and preclinical data needed '...
+            'to calculate the 5-SENSE score (N = %d).'],...
+            auc_stats(1,1),auc_stats(1,2),auc_stats(1,3),...
+            auc_stats(2,1),auc_stats(2,2),auc_stats(2,3),...
+            auc_stats(1,4),auc_stats(1,5),auc_stats(1,6),...
+            auc_stats(2,4),auc_stats(2,5),auc_stats(2,6),...
+            get_p_html(auc_stats(1,7)),get_p_html(auc_stats(2,7)),auc_stats(1,8));
+       
+
+        fprintf(fid,[' Results were similar when we used spikes detected in bipolar and machine references to '...
+            'build the SOZ laterality classifier (Fig. S4 and S5).</p>']);
     end
     
     %% Add subtitles
     annotation('textbox',[0 0.9 0.1 0.1],'String','A','LineStyle','none','fontsize',25)
-    annotation('textbox',[0.5 0.9 0.1 0.1],'String','B','LineStyle','none','fontsize',25)
-    annotation('textbox',[0 0.4 0.1 0.1],'String','C','LineStyle','none','fontsize',25)
-    annotation('textbox',[0.5 0.4 0.1 0.1],'String','D','LineStyle','none','fontsize',25)
-   
+    annotation('textbox',[0.33 0.9 0.1 0.1],'String','B','LineStyle','none','fontsize',25)
+    annotation('textbox',[0.67 0.9 0.1 0.1],'String','C','LineStyle','none','fontsize',25)
+    annotation('textbox',[0 0.4 0.1 0.1],'String','D','LineStyle','none','fontsize',25)
+    annotation('textbox',[0.35 0.40 0.1 0.1],'String','E','LineStyle','none','fontsize',25)
+    annotation('textbox',[0.67 0.4 0.1 0.1],'String','F','LineStyle','none','fontsize',25)
+    
     
     
     if ir == 1
